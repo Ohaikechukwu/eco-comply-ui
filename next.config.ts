0145fc8327980cd32adeadab66e6858fe5548e9e
Ikechukwu Oha <ikechukwuandrew4@gmail.com>
2026-04-22 11:07:19 +0100

    changed nextconfig to use proxie

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ hostname: "res.cloudinary.com" }],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: "https://ecocomply.ddns.net/api/v1/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
